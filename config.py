547e9467538052b964fbe1b55e95394dbe9bb875
gfgfgfgfgff <leakittydc@gmail.com>
2026-02-13 22:33:39 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# IDs des owners (ceux qui reçoivent les notifications)
+OWNER_IDS = [1399234120214909010, 1425947830463365120]
+
+# Token du bot (à mettre dans les secrets GitHub ou .env)
+BOT_TOKEN = "VOTRE_TOKEN_ICI"
