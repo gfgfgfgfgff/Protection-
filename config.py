f8f80d2fb41a7057542c6003a2eb3dfe8a78638e
gfgfgfgfgff <leakittydc@gmail.com>
2026-02-13 23:43:45 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 # IDs des owners (ceux qui reçoivent les notifications)
 OWNER_IDS = [1399234120214909010, 1425947830463365120]
 
+# IDs des admins (peuvent utiliser /savedb et /setdb)
+ADMIN_IDS = [1399234120214909010, 1425947830463365120]  # Même liste ou ajoute d'autres IDs
+
 # Token depuis les variables d'environnement Railway
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
