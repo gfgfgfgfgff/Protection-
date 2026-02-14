3760a411d75dd58cf5d5434d476c45e2edf186d4
gfgfgfgfgff <leakittydc@gmail.com>
2026-02-14 06:03:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
+# config.py
 import os
-
-# IDs des propriétaires du BOT (reçoivent les notifications)
 OWNER_IDS = [1399234120214909010, 1425947830463365120]
-
-# IDs des administrateurs (peuvent utiliser /savedb et /setdb)
-ADMIN_IDS = [1399234120214909010, 1425947830463365120]
-
-# Token depuis les variables d'environnement
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
 if not BOT_TOKEN:
-    raise ValueError("Le token BOT_TOKEN n'est pas défini dans les variables d'environnement")
+    raise ValueError("BOT_TOKEN non defini")
